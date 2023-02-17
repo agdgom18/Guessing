2a89e2b03a21471d4d6312c170b0ce7a6f798682
agdgom18 <agdgomelashvili18@gmail.com>
2023-02-17 19:42:51 +0400

    fix: guessinNumber style

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 'use strict';
 
 let secretNumber = Math.round(Math.random() * 10) + 1;
-
+console.log(secretNumber);
 let guessingNumber;
-console.log(guessingNumber);
+let highNumber = 0;
+let guessMessage = document.querySelector('.guess-message');
 
 const displayGuessMessage = function (message) {
   document.querySelector('.guess-message').textContent = message;
@@ -13,5 +14,14 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guessingNumber);
   if (!guessingNumber) {
     document.querySelector('.guess-message').textContent = 'Enter any number';
+  } else if (guessingNumber === secretNumber) {
+    document.querySelector('.question').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#fad0c4';
+    document.querySelector('body').style.width = '#50rem';
+    guessMessage.textContent = 'Congratulations';
+    if (score > highNumber) {
+      highNumber = score;
+      document.querySelector('.highscore').textContent = highNumber;
+    }
   }
 });
