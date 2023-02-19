5e51ae383b1a663e9397741c441c78805d0f8eae
agdgom18 <agdgomelashvili18@gmail.com>
2023-02-19 18:46:53 +0400

    fix: score issue

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
 'use strict';
 
-let secretNumber = Math.round(Math.random() * 10) + 1;
+let secretNumber = Math.round(Math.random() * 20) + 1;
 console.log(secretNumber);
 // let guessingNumber = Number(document.querySelector('.number-input').value);
-let highNumber = 0;
+let highscore = 0;
 let score = 20;
 let message = document.querySelector('.guess-message');
 
 document.querySelector('.check').addEventListener('click', function () {
   let guessingNumber = Number(document.querySelector('.number-input').value);
-  console.log(guessingNumber);
-  let scoreMessage = (document.querySelector('.score').textContent = score);
 
   if (!guessingNumber) {
     message.textContent = 'Enter a number';
@@ -20,10 +18,16 @@ document.querySelector('.check').addEventListener('click', function () {
     message.textContent = 'It is right';
     document.querySelector('.question').textContent = secretNumber;
     document.querySelector('.question').style.width = '50rem';
+    document.querySelector('.question').style.backgroundColor = '#B5A886';
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     // Player was wrong
   } else if (guessingNumber !== secretNumber) {
-    if (score > 0) {
+    if (score > 1) {
       guessingNumber > secretNumber
         ? (message.textContent = 'Too much')
         : (message.textContent = 'Too less');
@@ -33,4 +37,15 @@ document.querySelector('.check').addEventListener('click', function () {
       message.textContent = 'Game Over';
     }
   }
+});
+document.querySelector('.again').addEventListener('click', function () {
+  score = 20;
+  document.querySelector('.score').textContent = score;
+  document.querySelector('.question').style.width = '25rem';
+  document.querySelector('.question').textContent = '???';
+  message.textContent = 'Begin';
+
+  document.querySelector('.number-input').value = '';
+  secretNumber = Math.round(Math.random() * 20) + 1;
+  document.querySelector('.highscore').textContent = highscore;
 });
